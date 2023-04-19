a2fd35a1e05c7d36e1269fa0b74b1e74340ef70f
Khaled.emad <Khaled.emad@L-802-322>
2023-04-19 14:52:05 +0400

    Add project files.

---
+++ LoginITsupport/User.cs
@@ -0,0 +1,10 @@
+﻿namespace LoginITsupport
+{
+    public class User
+    {
+        public string username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }    
+
+    }
+}
